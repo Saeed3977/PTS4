edf97ad7368a96052cda40d37786df883e30256c
Joep Kerste <joep_ned@live.nl>
2015-03-29 16:21:31 +0200

    Added meal entry

--- HealthApp/app/src/main/java/com/pts4/healthapp/DiaryAddEntryActivity.java
+++ HealthApp/app/src/main/java/com/pts4/healthapp/DiaryAddEntryActivity.java
@@ -2,15 +2,19 @@ package com.pts4.healthapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.TypedValue;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 
 public class DiaryAddEntryActivity extends Activity {
+
+    ArrayList<String> items = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,26 @@ public class DiaryAddEntryActivity extends Activity {
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
         TextView timeField = (TextView)findViewById(R.id.timeValue);
         timeField.setText(sdf.format(new Date()));
+
+        populateFoodPicker();
+    }
+
+    private void populateFoodPicker()
+    {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.foodpickerrow, items);
+        ListView foodPicker = (ListView)findViewById(R.id.foodPicker);
+        foodPicker.setItemsCanFocus(false);
+        foodPicker.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        foodPicker.setAdapter(adapter);
+        items.add("Banana");
+        items.add("Butter");
+        items.add("White Bread");
+        items.add("Wholegrain Bread");
+        items.add("Tomato Sauce");
+        items.add("Milk");
+        items.add("Egg");
+        items.add("Cheese");
+        items.add("Pepper");
+        items.add("Salt");
     }
 }
