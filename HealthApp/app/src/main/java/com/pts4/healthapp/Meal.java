f4dd21c9ff54ff19a9a095f043fa2cea75f909cf
BuildTools <barterven@gmail.com>
2015-04-13 16:34:05 +0200

    Merge

--- HealthApp/app/src/main/java/com/pts4/healthapp/Meal.java
+++ HealthApp/app/src/main/java/com/pts4/healthapp/Meal.java
@@ -10,41 +10,39 @@ import java.util.List;
 /**
  * Created by Joep on 24-3-2015.
  */
-public class Meal extends SugarRecord<Meal>
-{
+public class Meal extends SugarRecord<Meal> {
     private String name;
     private String time;
     private String weight;
     private String calories;
     private String entryDate;
 
-    public Meal(){}
+    public Meal() {
+    }
 
-    public Meal(String name, String time, String weight, String calories)
-    {
+    public Meal(String name, String time) {
         super();
         this.name = name;
         this.time = time;
-        this.weight = weight;
-        this.calories = calories;
         this.entryDate = new Date().toString();
     }
 
-    public void addIngredient(Food food, int amount)
-    {
+    public void addIngredient(Food food, int amount) {
         Ingredient ing = new Ingredient(this, food, amount);
         ing.save();
         //ingredients.add(ing);
     }
 
-    public List<Ingredient> getIngredients()
-    {
+    /**
+     * Looks up all ingredients of the meal
+     *
+     * @return
+     */
+    public List<Ingredient> getIngredients() {
         ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
 
-        for(Ingredient i: Ingredient.listAll(Ingredient.class))
-        {
-            if (i.getMealID() == this.getId())
-            {
+        for (Ingredient i : Ingredient.listAll(Ingredient.class)) {
+            if (i.getMealID() == this.getId()) {
                 ingredients.add(i);
             }
         }
@@ -52,115 +50,88 @@ public class Meal extends SugarRecord<Meal>
         return Collections.unmodifiableList(ingredients);
     }
 
-    public String getName()
-    {
-        return name;
-    }
 
-    public String getTime()
-    {
-        return time;
-    }
-
-    /***
+    /**
      * Calculates the total weight of all ingredients
      *
      * @return The total weight of this meal.
      */
-    public int getWeight()
-    {
+    public int getWeight() {
         int result = 0;
 
-        for (Ingredient i : this.getIngredients())
-        {
+        for (Ingredient i : this.getIngredients()) {
             result += i.getAmount();
         }
 
         return result;
     }
 
-    /***
+    /**
      * Calculates the total of all calories in this meal
      *
      * @return The total of all calories in this meal.
      */
-    public int getCalories()
-    {
+    public int getCalories() {
         int result = 0;
 
-        for (Ingredient i : this.getIngredients())
-        {
+        for (Ingredient i : this.getIngredients()) {
             result += i.getAmount() * i.getFood().getCalories();
         }
 
         return result;
     }
 
-    /***
+    /**
      * Calculates the total of all proteins in this meal
      *
      * @return The total of all proteins in this meal.
      */
-    public int getProteins()
-    {
+    public int getProteins() {
         int result = 0;
 
-        for (Ingredient i : this.getIngredients())
-        {
+        for (Ingredient i : this.getIngredients()) {
             result += i.getAmount() * i.getFood().getProteins();
         }
 
         return result;
     }
 
-    /***
+    /**
      * Calculates the total of all fat in this meal
      *
      * @return The total of all fat in this meal.
      */
-    public int getFat()
-    {
+    public int getFat() {
         int result = 0;
 
-        for (Ingredient i : this.getIngredients())
-        {
+        for (Ingredient i : this.getIngredients()) {
             result += i.getAmount() * i.getFood().getFat();
         }
 
         return result;
     }
 
-    public String getEntryDate(){
+    public String getEntryDate() {
         return this.entryDate;
     }
 
-    public void setName(String name)
-    {
+    public String getName() {
+        return name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setTime(String time)
-    {
+    public void setTime(String time) {
         this.time = time;
     }
 
-    public void setWeight(String weight)
-    {
-        this.weight = weight;
-    }
-
-    public void setCalories(String calories)
-    {
-        this.calories = calories;
-    }
-
-//    public void setIngredients(ArrayList<Food> ingredients)
-//    {
-//        this.ingredients = ingredients;
-//    }
-
-    public void setEntryDate(String entryDate)
-    {
+    public void setEntryDate(String entryDate) {
         this.entryDate = entryDate;
     }
 }
