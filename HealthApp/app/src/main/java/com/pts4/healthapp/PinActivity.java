3268c8f51eb45cebbbe963fea1da17f3c6270cd6
Joep Kerste <joep_ned@live.nl>
2015-04-21 11:47:28 +0200

    Added PIN screen

---
+++ HealthApp/app/src/main/java/com/pts4/healthapp/PinActivity.java
@@ -0,0 +1,16 @@
+package com.pts4.healthapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class PinActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin);
+    }
+}
